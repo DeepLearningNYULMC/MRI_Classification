4508bb988aea31533b0ecc016f093d03e64a3788
narges razavian <narges.razavian@nyumc.org>
2017-05-24 14:50:10 -0400

    preprocessing scripts

---
+++ preprocessing/settings.py
@@ -0,0 +1,7 @@
+reference_file = '/mnt/d1/Brain/MRIdata_subset_T1AX_cp.xlsx'
+label_file = '/mnt/d1/Brain/TCGA_outcomes.xlsx'
+# home_data_dir='/mnt/d1/Brain/TCGA_GBM/'
+home_data_dir='/media/nsr3/NSR/TCIA/'
+list_subjects = ['DOI/TCGA-02-0003/1.3.6.1.4.1.14519.5.2.1.1706.4001.145725991542758792340793681239/1.3.6.1.4.1.14519.5.2.1.1706.4001.100169298880243060237139829068']
+
+output_data_dir = '/mnt/d1/Brain/numpy_t1ax/'
